5bb0003405351d236ff571a393d7ebb2d7d28770
NameOfFun <12damhao@gmail.com>
2024-12-24 16:43:56 +0700

    + add collision in tag

--- Assets/_Game/Scripts/MovingPlatform.cs
+++ Assets/_Game/Scripts/MovingPlatform.cs
@@ -31,5 +31,19 @@ public class MovingPlatform : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform, true);
+        }
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
 }
